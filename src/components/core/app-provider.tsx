f8c41cb062ec6899d04ad3be3dba2992be431c7e
KhenCahyo13 <khencahyo02@gmail.com>
2026-01-19 09:36:27 +0700

    ci: Install Tanstack router & query, and configured them

---
+++ src/components/core/app-provider.tsx
@@ -0,0 +1,34 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "@tanstack/react-router";
+import type { FC } from "react";
+
+import type { router } from "@/main";
+import type { LayoutProps } from "@/types/component";
+
+interface AppProviderProps extends LayoutProps {
+    queryClient: QueryClient;
+    appMode: string;
+    router: typeof router;
+}
+
+const AppProvider: FC<AppProviderProps> = ({
+    queryClient,
+    appMode,
+    children,
+    router,
+}) => (
+    <QueryClientProvider client={queryClient}>
+        {children}
+        {appMode === "development" && (
+            <ReactQueryDevtools
+                initialIsOpen={false}
+                position="bottom"
+                buttonPosition="bottom-right"
+            />
+        )}
+        <RouterProvider router={router} />
+    </QueryClientProvider>
+);
+
+export default AppProvider;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRouter } from '@tanstack/react-router';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { routeTree } from './routeTree.gen';
+import { QueryClient } from '@tanstack/react-query';
+import AppProvider from './components/core/app-provider';
+
+const APP_MODE = import.meta.env.VITE_APP_MODE;
+
+// Tanstack Query
+const queryClient = new QueryClient();
+
+// Tanstack Router
+export const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('root')!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <AppProvider
+        queryClient={queryClient}
+        appMode={APP_MODE}
+        router={router}
+      />
+    </StrictMode>
+  );
+}
