de4fa4894e59f42225fbb97d339c6355bbf478b0
KhenCahyo13 <khencahyo02@gmail.com>
2026-01-19 11:12:12 +0700

    ci: ESLint & Prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -3,21 +3,37 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores([
+        'dist',
+        'vite.config.ts',
+        'src/components/ui/**/*.tsx',
+        'src/components/tanstack-form/**/*.tsx',
+        'src/components/blocks/**/*.tsx',
+        'src/components/editor/**/*.tsx',
+    ]),
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs.flat.recommended,
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            'max-lines': ['error', { max: 500 }],
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
     },
-  },
 ])
